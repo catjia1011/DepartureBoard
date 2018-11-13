3a2450bb94f4b9d98e496edc0e6c7169b5687d2b
Cat Jia <catjia1011@gmail.com>
2018-11-13 23:53:24 +0800

    Update UI of main page

--- Departure Board/MainViewController.swift
+++ Departure Board/MainViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
+    private enum Section { case settings, edit }
+    private let sectionValues: [Section] = [.settings, .edit]
+
     var settings: [AppSettings.StationAndDirection] = []
+
+    init() {
+        super.init(style: .grouped)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,29 +45,58 @@ class MainViewController: UITableViewController {
         self.settings = AppSettings.getSettings()
         self.tableView.reloadData()
     }
+}
 
+extension MainViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionValues.count
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return settings.count
+        switch sectionValues[section] {
+        case .settings:
+            return settings.count
+        case .edit:
+            return 1
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeue(cellType: UITableViewCell.self, for: indexPath)
-        let setting = settings[indexPath.row]
-        let station = setting.lineStation.station
-        let line = setting.lineStation.line
-        let direction = setting.direction
-        cell.textLabel?.text = "\(station.name) → \(line.destinationName(for: direction))"
-        return cell
+        switch sectionValues[indexPath.section] {
+        case .settings:
+            let cell = tableView.dequeue(cellType: UITableViewCell.self, for: indexPath)
+            let setting = settings[indexPath.row]
+            let station = setting.lineStation.station
+            let line = setting.lineStation.line
+            let direction = setting.direction
+            cell.textLabel?.text = "\(station.name) → \(line.destinationName(for: direction))"
+            cell.textLabel?.textColor = .black
+            return cell
+
+        case .edit:
+            let cell = tableView.dequeue(cellType: UITableViewCell.self, for: indexPath)
+            cell.textLabel?.text = "添加或移除車站"
+            cell.textLabel?.textColor = .appleBlue
+            return cell
+        }
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-    }
 
+        switch sectionValues[indexPath.section] {
+        case .settings:
+            break
+
+        case .edit:
+            let vc = UINavigationController(rootViewController: StationListViewController())
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+}
+
+extension MainViewController {
     @objc private func didTapEditButtonItem(_ sender: Any) {
-        let vc = UINavigationController(rootViewController: StationListViewController())
-        self.present(vc, animated: true, completion: nil)
     }
 
     @objc private func didReceiveNotification(_ notification: Notification) {
